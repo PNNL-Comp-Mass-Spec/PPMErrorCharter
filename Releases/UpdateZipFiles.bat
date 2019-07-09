a429a168768224a7d9839e1b9582d969a0ba92d8
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-08 19:09:54 -0700

    Add a batch file to update the distribution .zip files

---
+++ Releases/UpdateZipFiles.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\7-Zip\7z.exe" a PPMErrorCharter_Program.zip ..\PPMErrorCharter\bin\Debug\PPMErrorCharter.exe ..\PPMErrorCharter\bin\Debug\PRISM.dll ..\PPMErrorCharter\bin\Debug\PSI_Interface.dll ..\PPMErrorCharter\bin\Debug\OxyPlot.dll ..\PPMErrorCharter\bin\Debug\OxyPlot.Wpf.dll ..\Readme.md  
+
+"C:\Program Files\7-Zip\7z.exe" a PPMErrorCharterPython_Program.zip ..\PPMErrorCharter\bin\PythonPlot\PPMErrorCharterPython.exe ..\PPMErrorCharter\bin\PythonPlot\PRISM.dll ..\PPMErrorCharter\bin\PythonPlot\PSI_Interface.dll ..\Readme.md  
+
+pause
